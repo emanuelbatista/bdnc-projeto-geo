f17f955beed927f4ada5608bb08a48d27df8cca6
dijalmasilva <dijalmacz@gmail.com>
2015-05-08 21:19:47 -0300

    Icone Search no lado esquerdo, e ao clicar aparece o search

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
+    
+    var searchFocus = false;
+    
 	jQuery('#maps').hide();
 	jQuery('#start').click(function(){
 		jQuery('#init').fadeOut(1000);
 		jQuery('#maps').fadeIn(1000);
+                jQuery('#address').hide();
+                
+                
+                jQuery('#search').click(function (){
+                    jQuery('#search').fadeOut(500);
+                    jQuery('#address').fadeIn(400);
+                    
+                    document.getElementById('#address').focus();
+                });     
 	});
+        
+        
 });
