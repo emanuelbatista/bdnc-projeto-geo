450ad6d290ce556afab5b518c259a4258e9bc751
dijalmasilva <dijalmacz@gmail.com>
2015-05-09 00:06:48 -0300

    Efeitos para fechar o model com ESC e apertando no X

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,26 +1,54 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     var searchFocus = false;
-    
-	jQuery('#maps').hide();
-	jQuery('#start').click(function(){
-		jQuery('#init').fadeOut(1000);
-		jQuery('#maps').fadeIn(1000);
-                jQuery('#address').hide();
-                
-                
-                jQuery('#search').click(function (){
-                    jQuery('#search').fadeOut(500);
-                    jQuery('#address').fadeIn(400);
-                    jQuery('#address').focus();
-                }); 
-                
-                
-                jQuery('#address').blur(function (){
-                   jQuery('#address').fadeOut(500);
-                   jQuery('#search').fadeIn(400);
-                });
-	});
-        
-        
+
+    jQuery('#maps').hide();
+    jQuery('#start').click(function () {
+        jQuery('#init').fadeOut(1000);
+        jQuery('#maps').fadeIn(1000);
+        jQuery('#address').hide();
+        jQuery('#addImageForm').hide();
+
+        jQuery('#search').click(function () {
+            jQuery('#search').fadeOut(500);
+            jQuery('#address').fadeIn(400);
+            jQuery('#address').focus();
+            esconderAddImageForm();
+        });
+
+        function esconderAddress() {
+            jQuery('#address').fadeOut(500);
+            jQuery('#search').fadeIn(400);
+        }
+
+        function esconderAddImageForm() {
+            jQuery('#addImageForm').fadeOut(500);
+            jQuery('#add').fadeIn(400);
+        }
+
+        jQuery('#address').blur(function () {
+            esconderAddress();
+        });
+
+        jQuery('#add').click(function () {
+            jQuery('#add').fadeOut(500);
+            jQuery('#addImageForm').fadeIn(400);
+            jQuery('#authors').focus();
+
+            $('body').keydown(function (e) {
+
+                if (e.which === 27) {
+                    esconderAddImageForm();
+                }
+            });
+        });
+
+        jQuery('#fechar').click(function () {
+            esconderAddress();
+            esconderAddImageForm();
+        });
+
+    });
+
+
 });
